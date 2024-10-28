50dc141388bf1a6cae828dc6ffe4db231ae409d1
Robert Ortiz <84008858+last-orbit@users.noreply.github.com>
2024-10-28 16:54:33 +0100

    Day 1 Questions Solved

--- src/question.js
+++ src/question.js
@@ -1,7 +1,53 @@
 class Question {
     // YOUR CODE HERE:
     //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty) {
+        this.text = text
+        this.choices = choices
+        this.answer = answer
+        this.difficulty = difficulty
+    }
 
-    // 2. shuffleChoices()
-}
+    // const shuffleArray = array => {
+    //     for (let i = array.length - 1; i > 0; i--) {
+    //       const j = Math.floor(Math.random() * (i + 1));
+    //       const temp = array[i];
+    //       array[i] = array[j];
+    //       array[j] = temp;
+    //     }
+    // }
+
+
+    // shuffleChoices(choices) {
+    //         for (let i = choices.length - 1; i > 0; i--) {
+    //           const j = Math.floor(Math.random() * (i + 1));
+    //           [choices[i], choices[j]] = [choices[j], choices[i]];
+    //         }
+
+    //         return choices;
+    //     }
+     shuffleChoices() {
+         // Fisher-Yates Shuffle
+        for (let i = this.choices.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.choices[i], this.choices[j]] = [
+              this.choices[j],
+              this.choices[i],
+            ];
+        }
+
+        return this.choices;
+    }
+};
+
+// function shuffleChoices(choices) {
+//     // Fisher-Yates Shuffle
+//     for (let i = choices.length - 1; i > 0; i--) {
+//       const j = Math.floor(Math.random() * (i + 1));
+//       [choices[i], choices[j]] = [choices[j], choices[i]];
+//       console.log("j", j);
+//       console.log([choices[i], choices[j]]);
+//     }
+
+//     return choices;
+//   }
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+    // shuffleChoices(choices) {
+    //         for (let i = choices.length - 1; i > 0; i--) {
+    //           const j = Math.floor(Math.random() * (i + 1));
+    //           [choices[i], choices[j]] = [choices[j], choices[i]];
+    //         }
+
+    //         return choices;
+    //     }
